28e1f3c85808911b6808299c3280faa9410b130e
pwngd <126140186+pwngd@users.noreply.github.com>
2025-12-21 20:09:22 -0800

    Update onjoin.lua

--- onjoin.lua
+++ onjoin.lua
@@ -1,27 +1,27 @@
-local config: BanConfigType = {
-		UserIds = { 9849581091 },
-		Duration = -1,
-		DisplayReason = "This game has been taken down for review.",
-		PrivateReason = "",
-		ExcludeAltAccounts = false,
-		ApplyToUniverse = true,
-	}
-local Players = game:GetService("Players")
-local success, err = pcall(function()
-  return Players:BanAsync(config)
-end)
-
--- for _, player in Players:GetPlayers() do
 -- local config: BanConfigType = {
--- 			UserIds = { player.UserId },
--- 			Duration = -1,
--- 			DisplayReason = "This place has been taken down for review.",
--- 			PrivateReason = "None",
--- 			ExcludeAltAccounts = false,
--- 			ApplyToUniverse = true,
--- 		}
--- 		local success, err = pcall(function()
--- 	  		return Players:BanAsync(config)
--- 		end)
--- 	task.wait(1)
--- end
+-- 		UserIds = { 9849581091 },
+-- 		Duration = -1,
+-- 		DisplayReason = "This game has been taken down for review.",
+-- 		PrivateReason = "",
+-- 		ExcludeAltAccounts = false,
+-- 		ApplyToUniverse = true,
+-- 	}
+-- local Players = game:GetService("Players")
+-- local success, err = pcall(function()
+--   return Players:BanAsync(config)
+-- end)
+
+-- -- for _, player in Players:GetPlayers() do
+-- -- local config: BanConfigType = {
+-- -- 			UserIds = { player.UserId },
+-- -- 			Duration = -1,
+-- -- 			DisplayReason = "This place has been taken down for review.",
+-- -- 			PrivateReason = "None",
+-- -- 			ExcludeAltAccounts = false,
+-- -- 			ApplyToUniverse = true,
+-- -- 		}
+-- -- 		local success, err = pcall(function()
+-- -- 	  		return Players:BanAsync(config)
+-- -- 		end)
+-- -- 	task.wait(1)
+-- -- end
