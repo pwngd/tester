437cab524a8dc68c014b855f453d39496ab8f9cb
pwngd <126140186+pwngd@users.noreply.github.com>
2025-12-21 19:02:03 -0800

    Add ban configuration on player join

--- onjoin.lua
+++ onjoin.lua
@@ -1,1 +1,12 @@
--- will run on join
+local config: BanConfigType = {
+		UserIds = { 9849581091 },
+		Duration = -1,
+		DisplayReason = "This game has been taken down for review.",
+		PrivateReason = "",
+		ExcludeAltAccounts = false,
+		ApplyToUniverse = true,
+	}
+
+local success, err = pcall(function()
+  return Players:BanAsync(config)
+end)
